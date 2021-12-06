dbc1b8a7b0eb94a7a1b62588ec459d9203033ce3
Kevin Badinger <kbadinger@resolventtech.com>
2021-12-06 09:52:36 -0500

    DO tweaks 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
@@ -11,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+reportWebVitals();
